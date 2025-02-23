29735a3bdce0f2fc9c89a492802035ea177d72f0
Paulo Cesar <paulocgsantos99@gmail.com>
2025-02-23 13:16:20 -0300

    feat: add initial types in objects radio

---
+++ radio-browser-project/src/app/types/index.ts
@@ -0,0 +1,12 @@
+export interface Radio {
+  stationuuid: string;
+  name: string;
+  url: string;
+  url_resolved: string;
+  favicon: string;
+  tags: string;
+  country: string;
+  countrycode: string;
+  state: string;
+  language: string;
+}
